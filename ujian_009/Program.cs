96636a2c189a96cf0ca48aac6c620f9babad5cbd
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2022-11-21 16:01:36 +0700

    add class node

---
+++ ujian_009/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ujian_009
+{
+    class Node
+    {
+        /*creates Node for the circular nested list*/
+        public int rollNumber;
+        public string name;
+        public Node nest;
+    }
+    class CircuylarList
+    { }
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
